64c9c6d143a866edcf037f1f329f63c6e5e2f6b1
susuupup <xinshen02017@163.com>
2025-02-19 12:37:14 +0800

    更新 Vercel 部署配置

--- api/index.py
+++ api/index.py
@@ -1,4 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from main import app
 
+# 配置静态文件和模板
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # 导出 app 供 Vercel 使用
-export = app 
+app = app 
