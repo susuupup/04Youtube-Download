499fe9f37cb76fce8e1a6d49e4803e1a613c297f
susuupup <xinshen02017@163.com>
2025-02-19 14:05:11 +0800

    优化 YouTube 请求头信息

--- main.py
+++ main.py
@@ -118,12 +118,22 @@ async def download_video(video_url: str = Form(...)):
             'quiet': True,
             'no_warnings': True,
             'extract_info': True,
-            'cookiesfrombrowser': ('chrome',),  # 使用 Chrome 浏览器的 cookies
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language': 'en-us,en;q=0.5',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Referer': 'https://www.youtube.com/',
+                'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+                'Sec-Ch-Ua-Mobile': '?0',
+                'Sec-Ch-Ua-Platform': '"Windows"',
+                'Sec-Fetch-Dest': 'document',
                 'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'X-Youtube-Client-Name': '1',
+                'X-Youtube-Client-Version': '2.20240215.01.00'
             }
         }
 
